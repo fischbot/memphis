b5666ed8e6ea703951a641fd67d532aa6fb10146
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-05-22 22:35:45 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="page">
+        <header className="App-header">
+          <h1 className="logo-text">Memphis Live</h1>
+          <div className="search">
+
+          </div>
+        </header>
+        <main></main>
+        <footer>
+
+        </footer>
+      </div> // end .page
+    );
+  }
+}
+
+export default App;
