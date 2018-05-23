501c9fd4a5a94cb6319d605e8dda049066db613e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-05-22 23:38:14 -0400

    begin layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import logo from './assets/img/logo.png';
 
 class App extends Component {
   render() {
     return (
       <div className="page">
-        <header className="App-header">
-          <h1 className="logo-text">Memphis Live</h1>
+        <header>
+          <h1 className="site-name">Memphis Live</h1>
           <div className="search">
-
+            <input type="text" placeholder="search events" />
+            <button>Search</button>
           </div>
         </header>
-        <main></main>
-        <footer>
+        <main>
+          <section className="video">
+            <iframe width={854/2} height={480/2} src="https://www.youtube.com/embed/1nqTnMweTPE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          </section>
 
+          <section className="events">
+
+          </section>
+
+          <section className="newsletter-signup">
+              <div className="newsletter-form">
+                <p>Sign up to be notified of upcoming events</p>
+                <input type="text" placeholder="email address" />
+                <button>JOIN</button>
+              </div>
+          </section>
+        </main>
+        <footer>
+          <div className="footer-content">
+            <img src={logo} alt="Thriving Cities Logo"/>
+            <p>Copyright &copy; 2018 Thriving Cities</p>
+          </div>
         </footer>
-      </div> // end .page
+      </div>
     );
   }
 }
---
+++ src/events.js
@@ -0,0 +1,50 @@
+export const events [
+  {
+    id: 1,
+    title: 'Cursus Malesuada',
+    date: 'June 2',
+    time: '7pm - 11pm',
+    location: '123 Main St',
+    about: 'Maecenas sed diam eget risus varius blandit sit amet non magna.'
+  },
+  {
+    id: 2,
+    title: 'Magna',
+    date: 'June 4',
+    time: '6pm',
+    location: '456 Main St',
+    about: 'Nulla vitae elit libero, a pharetra augue.'
+  },
+  {
+    id: 3,
+    title: 'Bibendum Sit Ipsum',
+    date: 'June 9',
+    time: '1pm - 5pm',
+    location: '789 Main St',
+    about: 'Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur.'
+  },
+  {
+    id: 4,
+    title: 'Parturient',
+    date: 'June 12',
+    time: '7pm - 11pm',
+    location: '123 Main St',
+    about: 'Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.'
+  },
+  {
+    id: 5,
+    title: 'Condimentum',
+    date: 'June 14',
+    time: '6pm',
+    location: '456 Main St'
+  },
+  {
+    id: 6,
+    title: 'Condimentum',
+    date: 'June 17',
+    time: '1pm - 5pm',
+    location: '789 Main St',
+    about: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+  },
+
+];
