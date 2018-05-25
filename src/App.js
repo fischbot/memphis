90e6ad6408a79aa0e9726bfd5411e44dc5ee0d00
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-05-25 13:24:18 -0400

    WIP search functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import sampleEvent1 from './assets/img/sample-event1.png';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import { events } from './events';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events : events,
+      searchField : ""
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchField : event.target.value });
+  }
   render() {
+    {/* array of events from user search by event title */}
+    const filteredEvents = this.state.events.filter(event => {
+      return event.title.toLowerCase().includes(this.state.searchField.toLowerCase());
+    });
+
     return (
       <div className="page">{/* container for all site contents*/}
-        <Header />
-        <Main />
-        <Footer />
+        <Header /> {/* site name/logo */}
+        <Main filteredEvents={filteredEvents} /> {/* all main site content: video embed, event search, event list, newsletter sign up */}
+        <Footer /> {/* links (ex. about, contact, external, etc), company logo, copyright */}
       </div>
     );
   }
