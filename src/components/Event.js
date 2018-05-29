507b2bacb3f56830c265251bc2e0f8c59ad47877
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-05-29 10:50:07 -0400

    add 'require' to img src so images show up

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,7 @@ const Event = ({ title, img, date}) => {
   return (
     <div className="event">
       <h3 className="event-title">{title}</h3>
-      <img className="event-img fluid-img" src={`./assets/img/${img}`} alt="Sample Event"/>
+      <img className="event-img fluid-img" src={require(`../assets/img/${img}`)} alt="Sample Event"/>
       <p className="event-date">{date}</p>
       <button type="submit" className="event-signup-btn btn">Sign Up</button>
     </div>
