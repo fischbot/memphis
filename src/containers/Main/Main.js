d0716fcb5b87f6761713220d1fac4cc92acad4b1
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-05-29 15:20:00 -0400

    move carousel to its own component

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import EventList from '../../components/EventList';
 import SearchBox from '../../components/SearchBox';
 import NewsletterForm from '../../components/NewsletterForm';
+import Carousel from '../../components/Carousel';
 
 const Main = ({ filteredEvents }) => {
   return(
@@ -21,32 +22,7 @@ const Main = ({ filteredEvents }) => {
           <h2 className="section-heading">Featured Events</h2>
           <div className="row">
             <div className="col-12">
-              <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div className="carousel-inner">
-                  <div className="carousel-item active">
-                    <img className="d-block w-100" src="http://via.placeholder.com/800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide" />
-                  </div>
-                  <div className="carousel-item">
-                    <img className="d-block w-100" src="http://via.placeholder.com/800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide" />
-                  </div>
-                  <div className="carousel-item">
-                    <img className="d-block w-100" src="http://via.placeholder.com/800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide" />
-                  </div>
-                </div>
-                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Next</span>
-                </a>
-              </div>
+              <Carousel />
             </div>
           </div>
         </section>
