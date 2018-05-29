fb6b02cb60edeb6380885f172f34504b31713844
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-05-29 12:34:51 -0400

    add nav to header with upcoming events link

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -8,6 +8,13 @@ class Header extends Component {
           <div className="col-sm-12 col-md-6">
             <h1 className="site-name"><span>Memphis</span> Live</h1>
           </div>
+          <div className="">
+            <nav>
+              <ul>
+                <li><a href="#event-list">View Upcoming Events</a></li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </header>
     );
