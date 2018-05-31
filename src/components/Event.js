56197e7d56c419fcd819397024c103d57e689e8e
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-05-31 17:59:26 -0400

    remove name from sign up heading

--- src/components/Event.js
+++ src/components/Event.js
@@ -41,7 +41,7 @@ const Event = ({ name, img, date, time, location, about }) => {
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Sign up for {name}</h5>
+              <h5 class="modal-title" id="exampleModalLabel">Sign up</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
